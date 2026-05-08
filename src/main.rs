25095b5f1df6966a1503edaf7335f96e40bf05d5
Stephen Chryn <SRCthird@gmail.com>
2026-05-08 11:39:25 -0400

    process single row for testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod config;
 mod globals;
 mod reader;
+mod opc_ua_client;
+mod actions;
 
 use color_print::cformat;
 
 use crate::globals::Globals;
 use crate::reader::StdinReader;
+use crate::opc_ua_client::{OpcUaSession,LiveOpcUaClient};
 
 fn main() {
     let config  = config::Config::load(Globals::config_file());
@@ -20,7 +23,16 @@ fn main() {
         let prompt = cformat!("<white>{}</>", Globals::csv_request_path());
         stdreader.read_line(prompt)
     };
-
     println!("{}", csv_path);
+
+    let session = OpcUaSession::new(&config);
+    let mut client = LiveOpcUaClient { session: &session };
+
+    actions::process_row(&actions::CsvRow {
+        action: String::from("read"),
+        tag: String::from("Channel1.Device1.CycleStart"),
+        value: Some(String::from("true")),
+        sleep: 0,
+    }, 0, &mut client, &mut stdreader);
 
 }
